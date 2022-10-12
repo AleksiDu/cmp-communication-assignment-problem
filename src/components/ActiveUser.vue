1f4f8bb55c52698e85429dc9e826ff4995a64fc9
AleksiDu <aleksi.duluzauri@gmail.com>
2022-10-12 22:37:36 +0400

    Props and Custom Events

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="component">
+        <h2>{{ username }}</h2>
+        <h3>{{ age }}</h3>
+    </div>
+</template>
+  
+<script>
+export default {
+    props: ["username", "age"],
+};
+</script>
+  
+<style>
+.component {
+    width: 40%;
+}
+</style>
+  
---
+++ src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <label for="name">Name input</label>
+        <input name="name" v-model="name" @input="updateData">
+        <label for="name">Age input</label>
+        <input name="age" v-model="age" @input="updateData">
+    </div>
+</template>
+  
+<script>
+
+export default {
+    data() {
+        return {
+            name: '',
+            age: ''
+        }
+    },
+    methods: {
+        updateData() {
+            this.$emit('update-data', { name: this.name, age: this.age })
+        }
+    }
+
+}
+</script>
+  
+<style>
+.component {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+}
+</style>
